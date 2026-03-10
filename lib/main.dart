4e6bcd898ab7efba94491384bf6e044cf0354daa
Abhishek Singh <innabhishek677@gmail.com>
2026-03-10 20:05:56 +0530

    chore: migrate from flutter_dotenv to dart-define-from-file for Firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
@@ -31,7 +30,6 @@ void toggleAppTheme() {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: '.env');
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   configureDependencies();
   Bloc.observer = AppBlocObserver();
